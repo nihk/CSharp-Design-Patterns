0dde515b05c04215893b2bd06f4f70bab73f7ad6
nihk <nickjrose@gmail.com>
2015-12-23 17:11:41 -0500

    Removed utility classes

--- DecoratorPattern.StarbuzzCoffee/DecoratorPattern.StarbuzzCoffee/Utility Classes/BeverageSizes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DecoratorPattern.StarbuzzCoffee.Utility_Classes {
-    class BeverageSizes {
-        public const string Tall = "Tall";
-        public const string Grande = "Grande";
-        public const string Venti = "Venti";
-    }
-}
